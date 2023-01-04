93489237c252f6f38f0e42ba3f01e3ffd15b7ffc
ASHH <30630722+onii-bot@users.noreply.github.com>
2023-01-04 22:42:14 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def get_data():
+    response = requests.get("https://www.sharesansar.com/today-share-price")
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find("table")
+    rows = table.find_all("tr")
+    price_data = []
+    del rows[0]
+    for row in rows:
+        data = row.find_all("td")
+        company_name = data[1].text.strip()
+        company_close = data[6].text
+        price_data.append({"companyName": company_name, "closingPrice": company_close})
+
+    return price_data
+
+
+@app.route("/")
+def index():
+    return get_data()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+
