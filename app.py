788d462d412296bce00dfcd116b87d469ac85332
ASHH <30630722+onii-bot@users.noreply.github.com>
2023-01-05 12:09:28 +0545

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,39 @@ def get_data():
     return price_data
 
 
+def get_all_data():
+    response = requests.get("https://www.sharesansar.com/today-share-price")
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find("table")
+    rows = table.find_all("tr")
+    price_data = []
+    del rows[0]
+    for row in rows:
+        data = row.find_all("td")
+        price_data.append({"companyName": data[1].text.strip(),
+                           "stockConfidence": data[2].text.strip(),
+                           "openingPrice": data[3].text.strip(),
+                           "highestPrice": data[4].text.strip(),
+                           "lowestPrice": data[5].text.strip(),
+                           "closingPrice": data[6].text.strip(),
+                           "volumeWeightedAveragePrice": data[7].text.strip(),
+                           "volume": data[8].text.strip(),
+                           "previousClose": data[9].text.strip(),
+                           "turnover": data[10].text.strip(),
+                           "transactions": data[11].text.strip(),
+                           "difference": data[12].text.strip()
+                           })
+    return price_data
+
+
 @app.route("/")
 def index():
     return get_data()
+
+
+@app.route("/all")
+def all():
+    return get_all_data()
 
 
 if __name__ == "__main__":
